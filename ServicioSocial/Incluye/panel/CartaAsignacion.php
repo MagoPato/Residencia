e36331cf907b88c95ce5bf34490fd3b71f8d642f
David Patricio Escamilla Marquez <46275040+MagoPato@users.noreply.github.com>
2025-06-04 21:26:43 -0600

    Update CartaAsignacion.php

--- ServicioSocial/Incluye/panel/CartaAsignacion.php
+++ ServicioSocial/Incluye/panel/CartaAsignacion.php
@@ -118,6 +118,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <title>Generador de Carta de Asignación</title>
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <link href="../../css/styles.css" rel="stylesheet" />
     <link href="../../css/estilos-interfaz.css" rel="stylesheet" />
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
